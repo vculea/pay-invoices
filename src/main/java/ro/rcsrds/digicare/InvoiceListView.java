cb7ccbb964bf6e65578877fa9f9b153657548f89
PC <culeaviorel@gmail.com>
2016-09-30 21:33:16 +0300

    fix waiting after suma

--- src/main/java/ro/rcsrds/digicare/InvoiceListView.java
+++ src/main/java/ro/rcsrds/digicare/InvoiceListView.java
@@ -3,17 +3,23 @@ package ro.rcsrds.digicare;
 import com.sdl.selenium.conditions.Condition;
 import com.sdl.selenium.conditions.ConditionManager;
 import com.sdl.selenium.conditions.ElementRemovedSuccessCondition;
+import com.sdl.selenium.web.Position;
 import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.button.Button;
+import com.sdl.selenium.web.form.CheckBox;
 import com.sdl.selenium.web.link.WebLink;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Table;
 import com.sdl.selenium.web.utils.Utils;
 import org.openqa.selenium.InvalidElementStateException;
 
 public class InvoiceListView extends WebLocator {
     private WebLocator invoiceMenuItem = new WebLocator().setText("Facturi");
     private WebLink invoicesListItem = new WebLink().setText("Listă Facturi", SearchType.TRIM, SearchType.CHILD_NODE);
-    private Button selectAllButton = new Button().setText("Selectaţi tot").setVisibility(true);
+    private Table invoices = new Table().setId("invoices");
+    private Cell lastCell = invoices.getCell(1, new Cell(7, "neachitat")).setResultIdx(Position.LAST);
+    private CheckBox checkBox = new CheckBox(lastCell);
     private Button payAllButton = new Button().setText("Plătiţi factura", SearchType.STARTS_WITH);
     private WebLink seeInvoices = new WebLink().setAttribute("data-request", "/invoices-list");
 
@@ -39,11 +45,11 @@ public class InvoiceListView extends WebLocator {
         Condition condition = conditionManager.execute();
 
         closePromo.doClick();
-        if (seeInvoices.isElementPresent()) {
+        if (seeInvoices.isElementPresent() && !invoices.ready()) {
             seeInvoices.click();
         }
         closePromo.doClick();
-        selectAllButton.click();
+        checkBox.click();
     }
 
     public void payAll() {
